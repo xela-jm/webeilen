7a686af038aac359fbd8a74aa10f746066308a7f
Aleksei Gurylev <aleksei@gurylev.net>
2019-08-11 01:16:23 +0200

    Add some components to a header

--- app/src/components/layouts/headers/Header.js
+++ app/src/components/layouts/headers/Header.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import CurrencyProvider from "./CurrencyProvider";
+import SearchBox from "./SearchBox";
 
 class Header extends React.Component {
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <React.Fragment>
+                <CssBaseline />
+                <Container maxWidth="lg">
+                    <Button size="small">Sign In</Button>
+                    <Button size="small">Sign Up</Button>
+                    <CurrencyProvider></CurrencyProvider>
+                    <SearchBox />
+                    <div>
+                        number of items: 1
+                    </div>
+                    <div>
+                        total prise: 144
+                    </div>
+                </Container>
+            </React.Fragment>
         );
     }
 }
